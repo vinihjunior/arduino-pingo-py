9e600d8700b125a2603359b694b700f90ef83d08
vinihjunior <vinicius.dphelippe@gmail.com>
2016-07-18 21:02:25 -0300

    new folder and file (pushbutton)

---
+++ pushbutton/pushbutton.py
@@ -0,0 +1,16 @@
+import pingo
+
+board = pingo.detect.MyBoard()
+
+led_pin = board.pins[13]
+led_pin.mode = pingo.OUT
+
+button_pin = board.pins[7]
+button_pin.mode = pingo.IN
+
+
+while True:
+	if button_pin.state == pingo.HIGH:
+		led_pin.hi()
+	else:
+		led_pin.lo()
